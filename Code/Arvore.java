2cceb74a86b418ec4eea24765465cf54054875a4
acaciaterra <terra.acacia@gmail.com>
2015-05-13 20:38:08 -0300

    What am I doing?

--- Code/Arvore.java
+++ Code/Arvore.java
@@ -1,50 +1,58 @@
 class Arvore {
-	Nodo raiz;
-	
+	public Nodo raiz;
+	public static Nodo nil = new Nodo(0, false);
+
 	public Arvore (int n){
-		raiz = new Nodo(k);
+		this.raiz = new Nodo(n, false);
 	}
 
 	public void rotacao_esq (Nodo x) {
 		Nodo y = x.dir;
 		x.dir = y.esq;
-		if (y.esq != null) y.esq.p = x;
+		if (y.esq != Arvore.nil) y.esq.p = x;
 		y.p = x.p;
-		if (x.p == null) raiz = y;
+		if (x.p == Arvore.nil) raiz = y;
 		else if (x == x.p.esq) x.p.esq = y;
 		else x.p.dir = y;
 		y.esq = x;
 		x.p = y;
 	}
-	
+
 	public void rotacao_dir (Nodo x) {
 		Nodo y = x.esq;
 		x.esq = y.dir;
-		if (y.dir != null) y.dir.p = x;
+		if (y.dir != Arvore.nil) y.dir.p = x;
 		y.p = x.p;
-		if (x.p == null) raiz = y;
+		if (x.p == Arvore.nil) raiz = y;
 		else if (x == x.p.dir) x.p.dir = y;
 		else x.p.esq = y;
 		y.dir = x;
 		x.p = y;
 	}
-	
+
 	public void adiciona (int n) {
-		Nodo a = raiz.busca(n);
-		if (this.k > a.v) this.fixaadicao(a.dir = new Nodo(n, a));
-		else if (this.k < a.v) this.fixaadicao(a.esq = new Nodo(n, a));
+		Nodo a = this.busca(n);
+		if (this.v > a.v) {
+			a.dir = new Nodo(n, true);
+			a.dir.p = a;
+			this.fixaadicao(a.dir);
+		} else if (this.k < a.v) {
+			a.esq = new Nodo(n, true);
+			a.esq.p = a;
+			this.fixaadicao(a.esq);
+		}
 	}
-	
+
 	public void transplant (Nodo x, Nodo y) {
-		if (x.p == null) this.raiz = y;
+		if (x.p == Arvore.nil) this.raiz = y;
 		else if (x == x.p.esq) x.pai.esq = y;
 		else x.p.dir = v;
-		if (y != null) y.p = x.p;
+		if (y != Arvore.nil) y.p = x.p;
 	}
-	
+
 	public Nodo encontra (int n) {
 		Nodo a = raiz.busca(n);
-		if (a.v != n) return null;
+		if (a.v != n) return Arvore.nil;
 		else return a;
 	}
 }
--- Code/Nodo.java
+++ Code/Nodo.java
@@ -1,35 +1,40 @@
 class Nodo {
-	int v;
-	Nodo p, esq, dir;
-	boolean cor; // Se true, então é vermelho
-	
-	public Nodo(int n){
+	int v; // Valor
+	Nodo p, esq, dir; // Respectivamente, pai, esquerda e direita
+	boolean ver; // Se true, então é vermelho
+
+/*  public Nodo(int n){
 		this.v = n;
-		this.cor = false;
+		this.ver = false;
 		this.esq = this.dir = this.p = null;
+	} */
+
+
+	public Nodo(int n, boolean ver){
+		this.v = n;
+		this.ver = ver;
+		this.p = this.esq = this.dir = Arvore.nil;
 	}
-
-
-	public Nodo(int n, Nodo p){
-		this(n);
-		this.p = p;
-		this.cor = true;		
-	} 
 
 	public Nodo busca(int n){
-		if (this.n > this.v && this.dir != null) return this.dir.busca(n);
-		else if (this.n < this.v && this.esq != null) return this.esq.busca(n);
-		else return this;		
+		if (this.n > this.v && this.dir != Arvore.nil) return this.dir.busca(n);
+		else if (this.n < this.v && this.esq != Arvore.nil) return this.esq.busca(n);
+		else return this;
 	}
-	
+
 	public void mostra(){
-		System.out.println(this + ": " + this.esq + " <- (" + this.v + " + " + (this.cor ? "V" : "P") + ") -> " + this.dir);
-		if (this.esq != null) this.esq.mostra();
-		if (this.dir != null) this.dir.mostra();
+		System.out.println(this + ": " + this.esq + " <- (" + this.v + " + " + (this.ver ? "V" : "P") + ") -> " + this.dir);
+		if (this.esq != Arvore.nil) this.esq.mostra();
+		if (this.dir != Arvore.nil) this.dir.mostra();
 	}
-		
+
 	public Nodo minimo(){
-		if (this.esq != null) return esq.minimo();
-		else return this;		
-	}	
+		if (this.esq != Arvore.nil) return esq.minimo();
+		else return this;
+	}
+
+/*  public Nodo maximo(){
+		if (this.dir != Arvore.nil) return dir.minimo();
+		else return this;
+	} */
 }
